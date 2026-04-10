2b99912a468a2303cb2f1b3ac51feb76fda9bab6
Md Mahedi Hasan <mdmahedi6427@gmail.com>
2026-04-10 22:32:03 +0600

    adding the togle section

--- src/Components/AvalabalePlayers/AvailabalePlayers.jsx
+++ src/Components/AvalabalePlayers/AvailabalePlayers.jsx
@@ -4,7 +4,7 @@ const AvailabalePlayers = ({ players }) => {
   console.log(players);
 
   return (
-    <div className="mt-20">
+    <div className="mt-7.5">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {players.map((player, index) => {
           return <Card key={index} player={player}></Card>
--- src/Components/Navbar/Players/Players.jsx
+++ src/Components/Navbar/Players/Players.jsx
@@ -1,10 +1,44 @@
-import { use } from "react";
+import { use, useState } from "react";
 import AvailabalePlayers from "../../AvalabalePlayers/AvailabalePlayers";
 
 const Players = ({ playersPromise }) => {
   const players = use(playersPromise);
+  const [selectedType ,setSelectedType] = useState("available")
+  console.log(selectedType)
+
+
+
+
+
+
   return (
-    <div className=" container mx-auto">
+
+    <div className=" container mx-auto my-[60px] ">
+
+      <div className="flex justify-between gap-4 items-center " >
+        <h2 className='font-bold text-3xl'>Available Players</h2>
+
+        <div className="">
+
+          <button
+          onClick={()=>setSelectedType("available")}
+          className={`btn ${selectedType === "available" ? "bg-[#E7FE29]" : ""} rounded-r-none rounded-l-xl btn `}>Available</button>
+
+          <button
+          onClick={()=>setSelectedType("seleted")}
+           className={`btn ${selectedType === "seleted" ? "bg-[#E7FE29]" : ""} rounded-l-none rounded-r-xl btn `}>Seleted</button>
+
+        
+        </div>
+      </div>
+
+
+
+
+
+
+
+
       <AvailabalePlayers players={players}></AvailabalePlayers>
     </div>
   );
